afc347bbe9e964662c745f164e539c34a9370a9e
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-08 13:35:58 -0300

    update: implementation of controllers and middlewares of the new card route

--- src/routes/card.ts
+++ src/routes/card.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
+import { createCard } from '../controllers/card.js';
+import { validateSchema } from '../middlewares/newCard.js';
+
 const cardRouter = Router();
 
-cardRouter.post('/create'); // Create a new card
+cardRouter.post('/create', validateSchema, createCard); // Create a new card
 cardRouter.post('/activate'); // Activate a card
 cardRouter.get('/view'); // View a card
 cardRouter.get('/transactions'); // View transactions for a card
