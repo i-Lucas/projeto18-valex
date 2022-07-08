5cc453145143b272bdcd8f46e013d71ad16b972c
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-08 13:33:14 -0300

    update: middleware responsible for validating customer data

---
+++ src/middlewares/newCard.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+export async function validateSchema(req: Request, res: Response, next: NextFunction) {
+
+    const { employeeId, cardType } = req.body;
+    const { apikey } = req.headers;
+
+    if (!apikey) throw { status: 400, message: 'Missing API key' };
+    if (!employeeId) throw { status: 400, message: 'Missing employee identifier' };
+    if (!cardType) throw { status: 400, message: 'Missing card type' };
+
+    const cardTypeList = ['groceries', 'restaurants', 'transport', 'education', 'health'];
+    if (!cardTypeList.includes(cardType)) throw { status: 400, message: 'Invalid card type' };
+    next();
+}
