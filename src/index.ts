ffcf22f6f7d530ef609ddd954f080400b333fe36
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-08 10:15:50 -0300

    update: importing the error catcher

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 import dotenv from 'dotenv';
 import routes from './routes/index.js';
+import errorHandler from './middlewares/errorHandler.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 const port = + process.env.PORT || 3000;
 
