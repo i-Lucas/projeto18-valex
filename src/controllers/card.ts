0742bfb3bde3bb57c71f057200da236838d259d3
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-08 13:43:40 -0300

    update: full implementation of the functionality to create a new card

---
+++ src/controllers/card.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { TransactionTypes } from '../repositories/card.js';
+import { validateBusinessRules, createNewCard, convertTypes } from '../services/newCard.js';
+
+export async function createCard(req: Request, res: Response) {
+
+    let { apikey } = req.headers as any;
+    let { cardType, employeeId } = req.body;
+
+    const data = convertTypes(apikey, employeeId, cardType);
+    const result = await validateBusinessRules(data.apikey, data.employeeIdNumber, data.cardTypeString);
+    await createNewCard(result.employeeId, result.fullName, result.cardType as TransactionTypes);
+    res.sendStatus(201);
+}
