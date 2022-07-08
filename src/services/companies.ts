0857e6191138365c8a91a5c77251455239515fd5
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-08 13:38:16 -0300

    update: bank access and validation service functions for companies and employees

--- src/services/companies.ts
+++ src/services/companies.ts
@@ -1,7 +1,15 @@
 import * as companyRepository from '../repositories/company.js';
+import * as employeeRepository from '../repositories/employee.js';
 
 export async function validateCompany(apiKey: string) {
     const company = await companyRepository.findByApiKey(apiKey);
     if (!company) throw { status: 401, message: 'Invalid API key' };
     return company;
-}
+};
+
+export async function validateCompanyEmployee(companyId: number, employeeId: number) {
+
+    const employee = await employeeRepository.findById(employeeId);
+    if(employee.companyId !== companyId) throw { status: 404, message: 'This employee is not part of this company' };
+    return employee;
+};
