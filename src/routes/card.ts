5c697884a71dd2a37b70604104b51566432ad3b0
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-09 11:12:35 -0300

    update: transactions route

--- src/routes/card.ts
+++ src/routes/card.ts
@@ -8,7 +8,7 @@ const cardRouter = Router();
 cardRouter.post('/create', cardsMiddleware.build, cardsController.build); // Create a new card
 cardRouter.post('/activate', cardsMiddleware.activate, cardsController.activate); // Activate a card
 cardRouter.get('/view'); // View a card
-cardRouter.get('/transactions'); // View transactions for a card
+cardRouter.get('/transactions', cardsMiddleware.identifier, cardsController.balance); // View transactions for a card
 cardRouter.put('/block'); // Block a card
 cardRouter.put('/unlock'); // Unlock a card
 
