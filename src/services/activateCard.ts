f80debd29695eb121255f3758aed1eb2c7ab2a6a
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-09 10:14:56 -0300

    update: redistributing responsibilities]

--- src/services/activateCard.ts
+++ src/services/activateCard.ts
@@ -1,20 +1,19 @@
-import * as cardRepository from '../repositories/card.js';
+import * as cardService from './cards.js';
 import encryptionSystem from './encrypted.js';
 
 export async function validateActivateCardBusinessRules(cardCVV: string, password: string) {
 
     const encrypted = encryptionSystem.encrypted(cardCVV);
-    const card = await cardRepository.findByCVV(encrypted);
-    if (!card) throw { status: 404, message: 'Card not found' };
+    const card = await cardService.findCardByCvv(encrypted);
     if(!card.isBlocked) throw { status: 400, message: 'This card already activated' };
     const currentDate = new Date(new Date().getFullYear(), new Date().getMonth()).toISOString().substring(0, 7);
     if(currentDate > card.expirationDate) throw { status: 400, message: 'This card expired' };
     return card.id;
-}
+};
 
 export async function activateCardService(cardId: number, password: string) {
 
     const encrypted = encryptionSystem.encrypted(password);
-    await cardRepository.update(cardId, { isBlocked: false, password: encrypted });
-    return true;
-}
+    const result = await cardService.activateCard(cardId, encrypted);
+    return result ? true : false;
+};
--- src/services/cards.ts
+++ src/services/cards.ts
@@ -1,10 +1,20 @@
 import * as cardRepository from '../repositories/card.js';
-import { TransactionTypes } from '../repositories/card.js';
 
 export async function checkEmployeeAlreadyCardType(employeeId: number, cardType: string) {
 
-    const cardTypeEnum = cardType as TransactionTypes;
+    const cardTypeEnum = cardType as cardRepository.TransactionTypes;
     const card = await cardRepository.findByTypeAndEmployeeId(cardTypeEnum, employeeId);
     if (card.length !== 0) throw { status: 409, message: 'This employee already has a card of this type' };
     return card;
-}
+};
+
+export async function findCardByCvv(cvv: string) {
+    const card = await cardRepository.findByCVV(cvv);
+    if (!card) throw { status: 404, message: 'Card not found' };
+    return card;
+};
+
+export async function activateCard(cardId: number, password: string) {
+    await cardRepository.update(cardId, { isBlocked: false, password });
+    return true;
+};
