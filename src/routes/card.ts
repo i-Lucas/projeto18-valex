a0a97033ac77ae9e560e9bcd1c7215d91a0270b7
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-09 07:38:05 -0300

    update: implementation of controllers and middlewares

--- src/routes/card.ts
+++ src/routes/card.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
-import { createCard } from '../controllers/card.js';
+import { createCard, activateCard } from '../controllers/card.js';
 import { newCardSchema } from '../middlewares/newCard.js';
+import { activateSchema } from '../middlewares/activateCard.js';
 
 const cardRouter = Router();
 
 cardRouter.post('/create', newCardSchema, createCard); // Create a new card
-cardRouter.post('/activate'); // Activate a card
+cardRouter.post('/activate', activateSchema, activateCard); // Activate a card
 cardRouter.get('/view'); // View a card
 cardRouter.get('/transactions'); // View transactions for a card
 cardRouter.put('/block'); // Block a card
