e78c3fb56e25ea4553992e26e94e7a4e96b06180
i-Lucas <novo.contato.lucas@gmail.com>
2022-07-09 07:40:05 -0300

    update: change of semantic names and change of encryption technology

--- src/services/newCard.ts
+++ src/services/newCard.ts
@@ -1,13 +1,12 @@
 import { faker } from '@faker-js/faker';
-import Cryptr from 'cryptr';
-
+import encryptionSystem from './encrypted.js';
 import { validateCompany, validateCompanyEmployee } from './companies.js';
 import { validateEmployee } from './employee.js';
 import { checkEmployeeAlreadyCardType } from './cards.js';
 
 import * as cardRepository from '../repositories/card.js';
 
-export async function validateBusinessRules(apikey: string, employeeId: number, cardType: string) {
+export async function validateNewCardBusinessRules(apikey: string, employeeId: number, cardType: string) {
 
     const company = await validateCompany(apikey);
     const employee = await validateEmployee(employeeId);
@@ -20,15 +19,14 @@ export async function createNewCard(employeeId: number, cardholderName: string, 
 
     const cardNumber = faker.finance.creditCardNumber('63[7-9]#-####-####-###L');
     const cardCVV = faker.finance.creditCardCVV();
-    
-    const cryptr = new Cryptr(cardCVV);
+    const encrypted = encryptionSystem.encrypted(cardCVV);
     const expiration = new Date(new Date().getFullYear() + 5, new Date().getMonth()).toISOString().substring(0, 7);
 
     await cardRepository.insert({
         employeeId,
         number: cardNumber,
         cardholderName: getFormattedName(cardholderName),
-        securityCode: cryptr.encrypt(cardNumber),
+        securityCode: encrypted,
         expirationDate: expiration,
         password: 'isBlocked',
         isVirtual: false,
@@ -37,10 +35,7 @@ export async function createNewCard(employeeId: number, cardholderName: string, 
         type
     });
 
-    return {
-        cardNumber,
-        cardCVV
-    }
+    return { cardNumber, cardCVV };
 };
 
 export function convertTypes(apikey: string, employeeId: string, cardType: string) {
